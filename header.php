eaf91eb22089c576f8aa2345defae04fbe7ed852
LauraLjungqvist <49066364+LauraLjungqvist@users.noreply.github.com>
2019-05-29 10:13:11 +0200

    Add files via upload
    Added: footer.php, functions.php, header.php, index.php, logo-placeholder.png, sidebar.php and style.css

---
+++ header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Reclaimed</title>
+	<meta name="author" content="TehZoi">
+  	<meta name="description" content="Reclaimed, Horde guild in Classic World of warcraft" />
+  	<meta name="keywords" content="world of warcraft, guild, horde, reclaimed, classic, vanilla, wow, recruitment" />
+  	<meta name="robots" content="noindex,nofollow" />
+	<!-- fonts -->
+    <link rel="stylesheet" href="<?php echo "https://fonts.googleapis.com/css?family=Montserrat:300,400,700"; ?>" />
+    <link rel="stylesheet" href="<?php echo "https://use.fontawesome.com/releases/v5.8.1/css/all.css"; ?>" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+    <link href="style.css" rel="stylesheet" />
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+
+    <!-- favicon-->
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> style="font-family: 'Montserrat', sans-serif !important;">
+
+<!-- header -->
+<div class="header" id="header">
+
+
+  <nav class="navbar navbar-fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#" id="logo">
+          <span>RECLAIMED</span>
+          <img src="http://wordpress.lan/wp-content/uploads/2019/05/logo-placeholder.png" class="main-logo pull-left" style="margin-top:-15px;" alt="reclaimed placeholder logo">
+        </a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav nav-pills navbar-collapse" id="nav-menu">
+
+
+          <!-- navigation -->
+          <?php wp_nav_menu( array(
+            'theme_location' => 'header-menu',
+            'menu_class' => 'blog-nav list-inline'
+          ) ); ?>
+          <!-- /.navigation -->
+        </ul>
+        <!-- social -->
+        <ul class="nav navbar-nav nav-pills navbar-right hidden-sm hidden-xs pull-right" id="social">
+          <li><a href="#" target="_blank" rel="noopener noreferrer"><i class="fab fa-discord"></i> <span class="sr-only">Discord</span></a></li>
+          <li><a href="#" target="_blank" rel="noopener noreferrer"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" class="svg-inline--fa fa-instagram fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="15px" width="15px"><path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg> <span class="sr-only">Instagram</span></a></li>
+          <li><a href="#" target="_blank" rel="noopener noreferrer"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter" class="svg-inline--fa fa-twitter fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="15px" width="15px"><path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg> <span class="sr-only">Twitter</span></a></li>
+        </ul><!-- social -->
+
+
+      </div>
+    </div>
+  </nav>
+
+
+
+
+</div><!-- header -->
+
+<!-- main -->
+<div class="main" id="main">
